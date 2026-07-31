023afd7342d57e505a00eaf609a41e859341bafe
ennikyoungeun <ennikyoungeun@gmail.com>
2026-07-31 12:00:40 +0900

    스프링 프로젝트 신규생성, ViewResolver 설정 부분 확인

---
+++ SpringStudy8/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		
+		//--ViewResolver 설정이 없으면
+		//return "/WEB-INF/views/home.jsp";
+		//return "/WEB-INF/views/mainpage.jsp";
+		
+		//--ViewResolver 설정 이후
+		return "home";
+		//return "mainpage";
+		
+				//prefix : /WEB-INF/views/
+				//suffix : .jsp
+				
+				
+				//   /WEB-INF/views/home.jsp
+	}
+}
