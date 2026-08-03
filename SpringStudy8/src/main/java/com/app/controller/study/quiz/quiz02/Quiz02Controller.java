5ec58eea7a3237876a92c01698f1b7c5d7430f66
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-03 16:09:13 +0900

    퀴즈2 요청, 파라미터 전달

---
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz02/Quiz02Controller.java
@@ -0,0 +1,43 @@
+package com.app.controller.study.quiz.quiz02;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/quiz02")
+public class Quiz02Controller {
+
+	@RequestMapping("/req1")
+	public String req1() {
+		System.out.println("/quiz02/req1 GET 요청");
+		return "quiz/quiz02/req1";
+	}
+	
+	@PostMapping("/req2")
+	public String req2(HttpServletRequest request) {
+		System.out.println(request.getParameter("id"));
+		System.out.println(request.getParameter("name"));
+		System.out.println(request.getParameter("price"));
+		
+		System.out.println("/quiz02/req2 POST 요청");
+		return "quiz/quiz02/req2";
+	}
+	
+	@RequestMapping("/req3") //get,post 모두
+	public String req3(@RequestParam String p1,
+						 @RequestParam(required = false, defaultValue = "p2Default") String p2) {
+
+		//localhost:8080/quiz02/req3?p1=10&p2=40
+		System.out.println("/quiz02/req3 요청");
+	
+		
+		return "quiz/quiz02/req3";
+	}
+
+	
+}
+	
---
+++ SpringStudy8/src/main/webapp/WEB-INF/views/quiz/quiz02/req2.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1> Quiz02 req2 페이지</h1>
+	
+	<div>
+		<form action="/quiz02/req2" method="post">
+			id : <input type="text" name="id"> <br>
+			name : <input type="text" name="name"> <br>
+			price :  <input type="text" name="price"> <br>
+		</form>
+	</div>
+</body>
+</html>
