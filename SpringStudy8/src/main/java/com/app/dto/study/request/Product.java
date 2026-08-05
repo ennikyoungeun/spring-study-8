60b8b77bd236fdc9950dac01c14a108e90007d19
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-05 20:13:23 +0900

    JSTL 주요기능 사용법

--- SpringStudy8/src/main/java/com/app/dto/study/request/Product.java
+++ SpringStudy8/src/main/java/com/app/dto/study/request/Product.java
@@ -1,21 +1,38 @@
 package com.app.dto.study.request;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-//@Getter
-//@Setter
-//@ToString
-
-//한번에 처리하고 싶을때.
-@Data
 public class Product {
-	
+
 	public String id;
 	public String name;
 	public int price;
-	public String type;
+
+	public Product() {}
+	public Product(String id, String name, int price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
 	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
 }
