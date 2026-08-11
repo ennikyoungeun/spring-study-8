eccd36ddeea6f5ba6bfa74ebdfbcc202d17b037d
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-11 16:27:05 +0900

    비즈니스 요구사항에 따라 프로젝트 구조 및 파일 구성 연습

---
+++ SpringStudy8/src/main/java/com/app/controller/accommodation/AccommodationController.java
@@ -0,0 +1,17 @@
+package com.app.controller.accommodation;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AccommodationController {
+	
+	//숙박업소 관련 요청(request) 처리 흐름
+	
+	//업장세팅, 영업일 설정
+	//영업호실 등록/ 설정
+	// 각 호실면, 업장별 비용 세팅
+	
+	
+	//업장담당 관리자(사장) 들이 접속해서 관리/설정처리 ->AccommodationController
+	//고객들이 정보를 조회하는 페이지 -> RoomController
+}
