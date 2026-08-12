08cf66858e4614560e717a1db916116f4d7cc7b8
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-12 10:43:38 +0900

    Bean 등록 및 의존성 주입

--- SpringStudy8/src/main/java/com/app/controller/board/BoardController.java
+++ SpringStudy8/src/main/java/com/app/controller/board/BoardController.java
@@ -2,7 +2,9 @@ package com.app.controller.board;
 
 import org.springframework.stereotype.Controller;
 
-@Controller
+
+//study 쪽 BoardController중복
+//@Controller
 public class BoardController {
 	
 
