03a3ee17ef14a668e8455b8eded21fa6bbd69323
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-12 15:20:10 +0900

    퀴즈15 고객/객실 신규 등록 시나리오 | 관련 구조 구성하기

---
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz15/Quiz15Room.java
@@ -0,0 +1,10 @@
+package com.app.controller.study.quiz.quiz15;
+
+import lombok.Data;
+
+@Data
+public class Quiz15Room {
+	
+
+}
+
---
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz15/Quiz15RoomService.java
@@ -0,0 +1,9 @@
+package com.app.controller.study.quiz.quiz15;
+
+public interface Quiz15RoomService {
+
+	//객실정보를 저장!
+		public int saveRoom(Quiz15Room quiz15Room);
+		
+	
+}
