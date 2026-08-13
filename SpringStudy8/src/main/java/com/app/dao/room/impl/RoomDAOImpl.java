a5a553bb13f7914b9bff6f75f794fa7a1c894a6d
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-13 10:48:20 +0900

    DB 연동 신규 Room 등록 데이터 insert 저장,Mybatis-config 설정

--- SpringStudy8/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ SpringStudy8/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -2,6 +2,8 @@ package com.app.dao.room.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.app.dao.room.RoomDAO;
@@ -14,6 +16,10 @@ import com.app.dto.room.Room;
 //DAO Repository
 @Repository
 public class RoomDAOImpl implements RoomDAO {
+	
+	//DB연동 처리할때 필요한 관련된 객체(Bean) 주입
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
 
 	@Override
 	public List<Room> findRoomList() {
@@ -27,9 +33,13 @@ public class RoomDAOImpl implements RoomDAO {
 
 	@Override
 	public int saveRoom(Room room) {
-		// DB에 room정보 테이블에 room 정보를 저장
 		
-		return 0;
+		// DB에 room정보 테이블에 room 정보를 저장
+								//실행할 쿼리 위치의 식별자, 매개변수 
+		int result = sqlSessionTemplate.insert("room_mapper.saveRoom",room);
+		//수행 적용된 행의 수
+		
+		return result;
 	}
 	
 }
