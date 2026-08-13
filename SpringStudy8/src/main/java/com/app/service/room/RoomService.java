8cde958e6952f15b3ff87fd33bbfa73b7be7e17d
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-13 16:37:22 +0900

    관리자페이지 객실정보 삭제처리

--- SpringStudy8/src/main/java/com/app/service/room/RoomService.java
+++ SpringStudy8/src/main/java/com/app/service/room/RoomService.java
@@ -7,7 +7,9 @@ import com.app.dto.room.Room;
 public interface RoomService {
 	List<Room> findRoomList();
 	int saveRoom(Room room);
-	
+
 	Room findRoomByRoomId(int roomId);
+	
+	int removeRoom(int roomId);
 
 }
