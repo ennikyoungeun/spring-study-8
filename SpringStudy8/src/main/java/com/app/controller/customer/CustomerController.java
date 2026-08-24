2e97c59a84563fec82af9fb69cce534f2d3b5f60
ennikyoungeun <ennikyoungeun@gmail.com>
2026-08-24 12:06:47 +0900

    유효성 검증 3 (validator 인터페이스방식)

--- SpringStudy8/src/main/java/com/app/controller/customer/CustomerController.java
+++ SpringStudy8/src/main/java/com/app/controller/customer/CustomerController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +29,7 @@ import com.app.dto.user.User;
 import com.app.dto.user.UserDupCheck;
 import com.app.service.user.UserService;
 import com.app.util.LoginManager;
+import com.app.validator.UserValidator;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -48,6 +51,12 @@ public class CustomerController {
 		log.info("/customer/signup 경로 접근 확인");
 		
 		return "customer/signup";
+	}
+	
+	@InitBinder("user")
+	public void initUserBinder(WebDataBinder binder) {
+		UserValidator userValidator = new UserValidator();
+		binder.setValidator(userValidator);
 	}
 	
 	@PostMapping("/customer/signup") 
